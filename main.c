e3460b5a8493fca7acfb0f392415583c05dbbb51
Крюков Илья Петрович <iv822s10@bwp07.csc.local>
2019-03-30 12:29:24 +0700

    Final

--- main.c
+++ main.c
@@ -1,92 +1,66 @@
-#include <iostream>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-using namespace std;
-void Fillfg(char A[], char h[], int& i);
-void Fillfgs(char A[], char h[]);
-void Fillxch(char A[], char h[], int& i);
-void Fillych(char A[], char h[], int& i);
-void Fillrch(char A[], char h[], int& i);
-int main()
-{
-    char ch[100], xch[10], ych[10], rch[10], fg[10], f[7];
-    strcpy(f, "circle");
-    double s = 0, p = 0;
-    int i = 0, r;
-    printf("Vvedite 'circle(x,y,r)'\n \n");
-    cin >> ch;
-    memset(fg, 0, sizeof(fg));
-    memset(xch, 0, sizeof(xch));
-    memset(ych, 0, sizeof(ych));
-    memset(rch, 0, sizeof(rch));
-    Fillfg(fg, ch, i);
-    while (1) {
-        if (strcmp(f, fg) == 0) {
-            break;
-        }
-        memset(ch, 0, sizeof(ch));
-        memset(fg, 0, sizeof(fg));
-        printf("Error, please Enter 'circle(x,y,r)'\n");
-        cin >> ch;
-        Fillfgs(fg, ch);
-        puts(ch);
-    }
-    Fillxch(xch, ch, i);
-    Fillych(ych, ch, i);
-    Fillrch(rch, ch, i);
-    r = atoi(rch);
-    s = r * r * M_PI;
-    printf("Area = %.4f", s);
-    p = 2 * M_PI * r;
-    printf("\nPerimetr= %.4f\n", p);
+
+int readin(char* h, int i, char el) {
+	int k = 0, coor;
+	char ch[100];
+	memset(ch, 0, sizeof(ch));
+	while (h[i] != el) {
+		ch[k] = h[i];
+		k++;
+		i++;
+	}
+	coor = atoi(ch);
+	i++;
+	return coor;
 }
-void Fillfgs(char A[], char h[])
-{
-    int k = 0;
-    while (h[k] != '(') {
-        A[k] = h[k];
-        k++;
-    }
-}
-void Fillfg(char A[], char h[], int& i)
-{
-    int k = 0;
-    while (h[i] != '(') {
-        A[k] = h[i];
-        k++;
-        i = i + 1;
-    }
-    i += 1;
-}
-void Fillxch(char A[], char h[], int& i)
-{
-    int k = 0;
-    while (h[i] != ',') {
-        A[k] = h[i];
-        k++;
-        i = i + 1;
-    }
-    i += 1;
-}
-void Fillych(char A[], char h[], int& i)
-{
-    int k = 0;
-    while (h[i] != ',') {
-        A[k] = h[i];
-        k++;
-        i = i + 1;
-    }
-    i += 1;
-}
-void Fillrch(char A[], char h[], int& i)
-{
-    int k = 0;
-    while (h[i] != ')') {
-        A[k] = h[i];
-        k++;
-        i = i + 1;
-    }
-    i += 1;
+
+int main() {
+	int x, y, r, errortext, errornumber;
+	int i;
+	char h[1000], f1[] = {"circle"};
+	memset(h,' ',sizeof(h));
+	while (1) {
+		errortext = 0;
+		errornumber = 0;
+		printf("Please, enter 'circle(x,y,r)'\n");
+		gets(h);
+
+		for (i = 0; i < 6; i++) {
+			if (h[i] != f1[i]) {
+				errortext++;
+			}
+		}
+
+		while (1) {
+			if (h[i] == ',' || h[i] == '(' || h[i] == ')')
+				errornumber++;
+			i++;
+			if (h[i] == ' ') break;
+		}
+		if (errortext == 0 && errornumber == 4) {
+			i = 7;
+			x = readin(h, i, ',');
+			y = readin(h, i, ',');
+			r = readin(h, i, ')');
+		}
+		if (errortext == 0 && x != 0 && y != 0 && r != 0 &&
+		    errornumber == 4) {
+			double s, p;
+			s = r * r * M_PI;
+			p = 2 * M_PI * r;
+
+			printf("Type of Figure: ");
+			puts(f1);
+			printf("%d - X\n%d - Y\n%d - R\n", x, y, r);
+			printf("Area = %.4f\n", s);
+			printf("Perimeter= %.4f", p);
+
+			return 0;
+		} else {
+			printf("Error: unknown shape or wrong coordinates\n");
+		}
+	}
 }
